45e7bed3719706603a3f06377e229267a2dd5c44
BOVET Nicolas <nicolas.bovet@unifr.ch>
2018-05-19 16:55:35 +0200

    Add LEDs patterns

--- controllers/project/move.c
+++ controllers/project/move.c
@@ -7,6 +7,7 @@
 #include "../util/prox_sensors.h"
 #include "../util/ground_sensors.h"
 #include "../util/consts.h"
+#include "../util/leds.h"
 
 #include "move.h"
 
@@ -19,18 +20,16 @@
  * @param length The size of the array
  * @return The sum of each items from the array
  */
-double array_sum(const double array[], size_t length)
-{
+double array_sum(const double array[], size_t length) {
     double sum = 0;
-    for(size_t i = 0; i < length; ++i)
+    for (size_t i = 0; i < length; ++i)
         sum += array[i];
-    
+
     return sum;
 }
 
-void rotate(rotate_t direction)
-{
-    if(direction == ROTATE_LEFT)
+void rotate(rotate_t direction) {
+    if (direction == ROTATE_LEFT)
         motors_set_speed(-MOTOR_SPEED, MOTOR_SPEED);
     else
         motors_set_speed(MOTOR_SPEED, -MOTOR_SPEED);
@@ -38,23 +37,34 @@ void rotate(rotate_t direction)
 
 #define PROX_THRESHOLD 400
 
-void move_as(move_t behaviour)
-{
+void move_as(move_t behaviour) {
+    leds_set(false);
+
+    if (behaviour == MOVE_EXPLORER) {
+        led_set(4, true);
+        led_set(0, true);
+        led_set(1, true);
+        led_set(7, true);
+    } else if (behaviour == MOVE_LOVER) {
+        led_set(0, true);
+        led_set(1, true);
+        led_set(7, true);
+    }
+
     static const double lover_weights[PROX_COUNT] = {2, 2, 3, 4, 4, 3, 2, 2};
     static const double explorer_weights[PROX_COUNT] = {4, 3, 2, 1, 1, 2, 3, 4};
-    
+
     const double total_weight = array_sum(behaviour == MOVE_LOVER ? lover_weights : explorer_weights, PROX_COUNT);
-    
+
     double prox[2] = {0};
-    for(size_t i = 0; i < PROX_COUNT; ++i)
-    {
+    for (size_t i = 0; i < PROX_COUNT; ++i) {
         double weight = behaviour == MOVE_LOVER ? lover_weights[i] : explorer_weights[i];
         prox[i * 2 / PROX_COUNT] += weight * prox_get_value(i, true) / total_weight;
     }
-    
+
     double speed_right = MOTOR_SPEED - prox[behaviour == MOVE_LOVER ? 0 : 1] / PROX_THRESHOLD * MOTOR_SPEED;
     double speed_left = MOTOR_SPEED - prox[behaviour == MOVE_LOVER ? 1 : 0] / PROX_THRESHOLD * MOTOR_SPEED;
-    
+
     motors_set_speed(speed_left, speed_right);
 }
 
@@ -63,36 +73,39 @@ void move_as(move_t behaviour)
 #define PID_T_I 6
 #define PID_T_D 0.05
 
-double calculate_pid(double prox)
-{
+double calculate_pid(double prox) {
     static double error = 0;
     static double deriv = 0;
     static double integ = 0;
-    
+
     double prev_err = error;
-    
+
     error = prox - PID_WALL_FOLLOW_TARGET;
     deriv = (error - prev_err) * 1000 / TIME_STEP;
     integ += error * TIME_STEP / 1000;
-    
+
     return PID_K * error + PID_K * PID_T_D * deriv + PID_K / PID_T_I * integ;
 }
 
-void follow_wall()
-{
+void follow_wall() {
+    leds_set(false);
+    led_set(5, true);
+    led_set(6, true);
+    led_set(7, true);
+
     static const double weights[PROX_COUNT] = {3, 1, 1, 0, 0, 0, 0, 0};
-    
+
     double prox_right = 0;
-    for(size_t i = 0; i < PROX_COUNT; ++i)
+    for (size_t i = 0; i < PROX_COUNT; ++i)
         prox_right += weights[i] * prox_get_value(i, true);
-    
+
     prox_right /= array_sum(weights, PROX_COUNT);
-    
+
     double ds = calculate_pid(prox_right);
-    
+
     double speed_right = (fabs(ds) > 1 ? 0 : MOTOR_SPEED) + ds;
     double speed_left = (fabs(ds) > 1 ? 0 : MOTOR_SPEED) - ds;
-    
+
     motors_set_speed(speed_left, speed_right);
 }
 
@@ -100,15 +113,19 @@ void follow_wall()
 #define GROUND_FACTOR 250
 #define GROUND_SPEED 1
 
-void follow_line()
-{
+void follow_line() {
+    leds_set(false);
+    led_set(3, true);
+    led_set(4, true);
+    led_set(5, true);
+    led_set(6, true);
     static const double right_weights[GROUND_COUNT] = {0, 0, 1};
     static const double left_weights[GROUND_COUNT] = {1, 0, 0};
-    
+
     double ground_right = GROUND_MAX - ground_get_value(GROUND_RIGHT);
     double ground_center = GROUND_MAX - ground_get_value(GROUND_CENTER);
     double ground_left = GROUND_MAX - ground_get_value(GROUND_LEFT);
-    
+
     double right_factor =
             ground_right * right_weights[GROUND_RIGHT] +
             ground_center * right_weights[GROUND_CENTER] +
@@ -117,64 +134,60 @@ void follow_line()
             ground_right * left_weights[GROUND_RIGHT] +
             ground_center * left_weights[GROUND_CENTER] +
             ground_left * left_weights[GROUND_LEFT];
-    
+
     right_factor /= array_sum(right_weights, GROUND_COUNT);
     left_factor /= array_sum(left_weights, GROUND_COUNT);
-    
+
     double speed_right = GROUND_SPEED + left_factor / GROUND_FACTOR * GROUND_SPEED;
     double speed_left = GROUND_SPEED + right_factor / GROUND_FACTOR * GROUND_SPEED;
-    
+
     motors_set_speed(speed_left, speed_right);
 }
 
 #define GROUND_THRESHOLD 500
 
-bool detects_line()
-{
+bool detects_line() {
     double ground_value =
             ground_get_value(GROUND_LEFT) +
             ground_get_value(GROUND_CENTER) +
             ground_get_value(GROUND_RIGHT);
-    
+
     return (ground_value / GROUND_COUNT) < GROUND_THRESHOLD;
 }
 
 #define WALL_THRESHOLD 800
 
-bool detects_wall(wall_t side)
-{
+bool detects_wall(wall_t side) {
     static const size_t sensors[4][2] = {
             {5, 6}, // WALL_LEFT
             {1, 2}, // WALL_RIGHT
             {7, 0}, // WALL_FRONT
             {3, 4}  // WALL_BACK
     };
-    
+
     double prox_value = prox_get_value(sensors[side][0], true) + prox_get_value(sensors[side][1], true);
     return prox_value / 2 > WALL_THRESHOLD;
 }
 
-void avoid_lines()
-{
-    typedef enum
-    {
+void avoid_lines() {
+    typedef enum {
         SENSOR_LEFT, SENSOR_RIGHT, SENSOR_NONE
     } sensor_t;
-    
+
     static sensor_t sensor = SENSOR_NONE;
-    
-    if(sensor == SENSOR_LEFT && ground_get_value(GROUND_LEFT) > GROUND_THRESHOLD)
+
+    if (sensor == SENSOR_LEFT && ground_get_value(GROUND_LEFT) > GROUND_THRESHOLD)
         sensor = SENSOR_NONE;
-    if(sensor == SENSOR_RIGHT && ground_get_value(GROUND_RIGHT) > GROUND_THRESHOLD)
+    if (sensor == SENSOR_RIGHT && ground_get_value(GROUND_RIGHT) > GROUND_THRESHOLD)
         sensor = SENSOR_NONE;
-    
-    if(sensor == SENSOR_NONE && ground_get_value(GROUND_LEFT) < GROUND_THRESHOLD)
+
+    if (sensor == SENSOR_NONE && ground_get_value(GROUND_LEFT) < GROUND_THRESHOLD)
         sensor = SENSOR_LEFT;
-    if(sensor == SENSOR_NONE && ground_get_value(GROUND_RIGHT) < GROUND_THRESHOLD)
+    if (sensor == SENSOR_NONE && ground_get_value(GROUND_RIGHT) < GROUND_THRESHOLD)
         sensor = SENSOR_RIGHT;
-    
-    if(sensor == SENSOR_LEFT)
+
+    if (sensor == SENSOR_LEFT)
         rotate(ROTATE_RIGHT);
-    if(sensor == SENSOR_RIGHT)
+    if (sensor == SENSOR_RIGHT)
         rotate(ROTATE_LEFT);
 }
--- controllers/project/project.c
+++ controllers/project/project.c
@@ -14,8 +14,7 @@
 
 #define running wb_robot_step(TIME_STEP) != -1
 
-int main()
-{
+int main() {
     wb_robot_init();
     motors_init();
     leds_init();
@@ -23,15 +22,13 @@ int main()
     ground_init();
     camera_init();
     com_init();
-    
+
     color_t color_to_find = COLOR_ANY;
     color_t color_found = COLOR_NONE;
-    
-    while(running)
-    {
-        while(true)
-            switch(message_receive())
-            {
+
+    while (running) {
+        while (true)
+            switch (message_receive()) {
                 case MESSAGE_FIND_RED:
                     color_to_find = COLOR_RED;
                     break;
@@ -42,103 +39,108 @@ int main()
                     goto loop_end;
             }
         loop_end:;
-        
-        if(color_matches(color_to_find, color_found))
-        {
-            if(color_to_find == COLOR_BLUE)
+
+        if (color_matches(color_to_find, color_found)) {
+            if (color_to_find == COLOR_BLUE) {
+                leds_set(false);
+                led_set(0, true);
+                led_set(7, true);
                 break;
-            else
+            } else
                 continue;
-        }
-        
-        else if(color_found != COLOR_NONE && color_found != color_to_find)
-        {
+        } else if (color_found != COLOR_NONE && color_found != color_to_find) {
             color_found = COLOR_NONE;
             camera_init();
         }
-        
+
         color_t current_color = get_color();
-        
-        if(color_matches(color_to_find, current_color))
-        {
-            if(detects_wall(WALL_FRONT))
-            {
+
+        if (color_matches(color_to_find, current_color)) {
+            if (detects_wall(WALL_FRONT)) {
                 color_found = current_color;
-                
+                if (color_found == COLOR_BLUE) {
+                    leds_set(false);
+                    led_set(0, true);
+                    led_set(1, true);
+                    led_set(2, true);
+                } else if (color_found == COLOR_RED) {
+                    leds_set(false);
+                    led_set(0, true);
+                    led_set(6, true);
+                    led_set(7, true);
+                }
                 motors_stop();
                 camera_stop();
-                
+
                 message_send(current_color == COLOR_RED ? MESSAGE_FIND_BLUE : MESSAGE_FIND_RED);
-            }
-            else
+                if (message_send(MESSAGE_FIND_BLUE)){
+                    leds_set(false);
+                    led_set(1, true);
+                    led_set(3, true);
+                    led_set(5, true);
+                    led_set(7, true);
+                }
+            } else
                 move_as(MOVE_LOVER);
-        }
-        else
+        } else
             move_as(MOVE_EXPLORER);
-        
+
         avoid_lines();
     }
-    
-    while(running)
-    {
-        if(!detects_wall(WALL_RIGHT))
+
+    while (running) {
+        if (!detects_wall(WALL_RIGHT))
             rotate(ROTATE_LEFT);
         else
             break;
     }
-    
-    while(running)
-    {
+
+    while (running) {
         follow_wall();
-        
-        if(detects_line())
+
+        if (detects_line())
             break;
     }
-    
-    while(running)
-    {
+
+    while (running) {
         follow_wall();
-        
-        if(!detects_line())
+
+        if (!detects_line())
             break;
     }
-    
-    while(running)
-    {
-        if(ground_get_value(GROUND_RIGHT) > 500)
+
+    while (running) {
+        if (ground_get_value(GROUND_RIGHT) > 500)
             rotate(ROTATE_LEFT);
         else
             break;
     }
-    
-    while(running)
-    {
+
+    while (running) {
         follow_line();
-        
-        if(detects_wall(WALL_FRONT))
+
+        if (detects_wall(WALL_FRONT))
             break;
     }
-    
-    while(running)
-    {
-        if(!detects_wall(WALL_RIGHT))
+
+    while (running) {
+        if (!detects_wall(WALL_RIGHT))
             rotate(ROTATE_LEFT);
         else
             break;
     }
-    
-    while(running)
-    {
+
+    while (running) {
         follow_wall();
-        
-        if(detects_line())
+
+        if (detects_line())
             break;
     }
-    
+
     motors_stop();
     message_send(MESSAGE_FIND_BLUE);
-    
+
     wb_robot_cleanup();
-    
+
     return EXIT_SUCCESS;
 }
